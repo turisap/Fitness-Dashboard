a31dcf4b7e9eeb2e836ceb8591cfc73df5a682f6
Turisap <kirill-shakirov@mail.ru>
2018-01-20 16:15:33 +0700

    Fixes of erasing modal callback on close

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,11 +6,19 @@ import Rodal from 'rodal';
 import { connect } from 'react-redux';
 import { emptyModal } from '../actions/menuElements';
 
+
+/**
+ * Modal window which can be launched from anywhere in application
+ * By setting content to Redux store
+ * It also calls an callback (if provided) on closing
+ * @param props
+ * @constructor
+ */
 const Modal = props => (
     <Rodal visible={!!props.modalContent} onClose={() => {
         if(typeof(window.MODAL_CALLBACK) === 'function') {
             window.MODAL_CALLBACK();
-            void window.MODAL_CALLBACK;
+            window.MODAL_CALLBACK = null;
         }
         props.emptyModalContent();
     }}>
